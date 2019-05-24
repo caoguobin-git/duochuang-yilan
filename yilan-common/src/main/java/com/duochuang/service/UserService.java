172dce63d310a6939de67f61c87b272e359baaaf
caoguobin <caoguobin@live.com>
2019-05-24 17:21:20 +0800

    1111

---
+++ yilan-common/src/main/java/com/duochuang/service/UserService.java
@@ -0,0 +1,11 @@
+/***********************************************
+ * File Name: UserService
+ * Author: caoguobin
+ * mail: caoguobin@live.com
+ * Created Time: 24 05 2019 17:04
+ ***********************************************/
+package com.duochuang.service;
+
+public interface UserService {
+    String sayHello(String name);
+}
